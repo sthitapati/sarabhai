73edb3ea6d29266f342393ec65789173702dfbc1
sthitapati <sthita.pati@gmail.com>
2023-07-10 11:45:54 +0100

    added comments and help to main()

--- src/analyse_sequence_task.py
+++ src/analyse_sequence_task.py
@@ -1,9 +1,17 @@
 from utils.preprocess_utils import *
 
 def main():
+    """
+    Main function that processes animal data.
+    """
+    # Parse command line arguments
     arguments = parse_arguments()
+    
+    # Create a ConfigParser object and parse the metadata file
     metadata = ConfigParser(metadata_file=arguments['metadata'])
     metadata.parse_metadata()
+    
+    # Process the animal data
     process_animal_data(animal_ids= metadata.animal_ids,
                         input_directory=metadata.input_directory,
                         output_directory=metadata.output_directory,
@@ -11,6 +19,10 @@ def main():
                         replace_existing=arguments['replace']
                         )
 
-
 if __name__ == "__main__":
+    """
+    Entry point of the script. The script can be called using the command 
+    'python analyse_sequence_task.py -md (or --metadata) path_to_metadata_file -r (or --replace for if replace_existing is true)'
+    """
+    # Execute main function
     main()
